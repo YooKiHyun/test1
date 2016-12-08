be1b4861b5b4ad3c3d029cb1b4c06cfe9c3ae940
박상길@클라우드사업팀 <ghdrptkd@naver.com>
2016-12-08 10:34:09 +0900

    Master에서 바로

--- TestGit2/src/com/kcube/test2/Test2.java
+++ TestGit2/src/com/kcube/test2/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Test2");
 		System.out.println("Test Git Hub");
 		System.out.println("Test Git Hub222");
 	}
