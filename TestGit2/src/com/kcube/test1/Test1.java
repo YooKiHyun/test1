99487d7ff15cb07fce2ca2269a7ff5c11970b6d6
박상길@클라우드사업팀 <ghdrptkd@naver.com>
2016-12-08 10:40:02 +0900

    locale Branch(!Master) Commit And Push

--- TestGit2/src/com/kcube/test1/Test1.java
+++ TestGit2/src/com/kcube/test1/Test1.java
@@ -4,6 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Test1");
 		System.out.println("Test Git Hub");
 		System.out.println("Test Git Hub2");
 		System.out.println("Local Master Branch");
