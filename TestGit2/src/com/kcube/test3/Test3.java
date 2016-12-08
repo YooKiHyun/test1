0c70b9e35b51fb42cf35fd54dbda9f353b3f2c91
박상길@클라우드사업팀 <ghdrptkd@naver.com>
2016-12-08 10:12:34 +0900

    Test3 Package Add

---
+++ TestGit2/src/com/kcube/test3/Test3.java
@@ -0,0 +1,12 @@
+package com.kcube.test3;
+
+public class Test3
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Test Git Hub");
+	}
+
+}
